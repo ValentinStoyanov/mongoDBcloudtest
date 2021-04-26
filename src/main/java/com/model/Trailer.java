e796bb34f77cc08089e0afcbb9c6d2a51dbcf254
ValentinStoyanov <56118614+ValentinStoyanov@users.noreply.github.com>
2021-04-26 11:03:33 +0200

    languaje change

--- src/main/java/com/model/Trailer.java
+++ src/main/java/com/model/Trailer.java
@@ -3,7 +3,7 @@ package com.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "remolques")
+@Document(collection = "trailers")
 public class Trailer {
 	
 		
